2fa40c33c9b1437d5e95e79e41ab9f7c22d7a078
belplaton <platonbeliakov@gmail.com>
2025-03-24 05:00:33 +0300

    add gitignore

--- Graphs/belplaton.Graphs/obj/Debug/net7.0/belplaton.Graphs.AssemblyInfo.cs
+++ Graphs/belplaton.Graphs/obj/Debug/net7.0/belplaton.Graphs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("belplaton.Graphs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d1a5c4babcae2946240cd96c4cc4c2ad989d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("belplaton.Graphs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("belplaton.Graphs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
