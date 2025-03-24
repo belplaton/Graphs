cf1ba0ea79b6d07b6288ef69ca466e0674c467c4
belplaton <platonbeliakov@gmail.com>
2025-03-24 07:25:28 +0300

    Update GraphAlgorithms.cs

--- Graphs/belplaton.Graphs/GraphAlgorithms.cs
+++ Graphs/belplaton.Graphs/GraphAlgorithms.cs
@@ -35,4 +35,23 @@ public static class GraphAlgorithms
 			}
 		}
 	}
+	
+	public static List<List<TNode>> FindWeakConnectedComponents<TNode, TData>(IGraph<TNode, TData> graph)
+	{
+		var visited = new HashSet<TNode>();
+		var components = new List<List<TNode>>();
+
+		for (var i = 0; i < graph.Nodes.Count; i++)
+		{
+			var node = graph.Nodes[i];
+			if (!visited.Contains(node))
+			{
+				var component = new List<TNode>();
+				//smtng here
+				components.Add(component);
+			}
+		}
+
+		return components;
+	}
 }
