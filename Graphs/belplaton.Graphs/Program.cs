7e06bdc94b726550d204d580e16290b7e9b5762e
belplaton <platonbeliakov@gmail.com>
2025-03-24 04:00:11 +0300

    Update Program.cs

--- Graphs/belplaton.Graphs/Program.cs
+++ Graphs/belplaton.Graphs/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using belplaton.Graphs;
+
+internal static class Program
+{
+	public static void Main(string[] args)
+	{
+		const string filePath = "graph_input.txt";
+		if (!File.Exists(filePath))
+		{
+			Console.WriteLine($"file'{filePath}' is not found!");
+			return;
+		}
+		
+		var lines = File.ReadAllLines(filePath);
+		var builder = new NumericGraphBuilder<int>();
+		builder.ParsePayloadInput<RibsListNumericGraphInputParser>(lines);
+		
+		var graph = builder.CreateGraph();
+		
+		Console.WriteLine(graph.ToString());
+		
+		Console.WriteLine("any key to clos...");
+		Console.ReadKey();
+	}
+}
