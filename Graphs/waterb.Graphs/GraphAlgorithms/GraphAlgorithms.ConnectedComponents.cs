77014fddca33833dc31c50c0d6a3c169c19b7c37
belplaton <platonbeliakov@gmail.com>
2025-03-31 01:28:31 +0300

    Update GraphAlgorithms.ConnectedComponents.cs

--- Graphs/waterb.Graphs/GraphAlgorithms/GraphAlgorithms.ConnectedComponents.cs
+++ Graphs/waterb.Graphs/GraphAlgorithms/GraphAlgorithms.ConnectedComponents.cs
@@ -162,4 +162,97 @@ public static partial class GraphAlgorithms
 			}
 		}
 	}
+
+	public static (List<TNode> joints, List<(TNode from, TNode to)> bridges)? FindJointsAndBridges<TNode, TData>(
+		this IGraph<TNode, TData> graph, ref HashSet<TNode>? visited,
+		ref Stack<DFSEnumerator<TNode, TData>.DFSNode>? stack) where TNode : notnull
+	{
+		if (graph.Size == 0) return null;
+		
+		(visited ??= new HashSet<TNode>()).Clear();
+		(stack ??= new Stack<DFSEnumerator<TNode, TData>.DFSNode>()).Clear();
+
+		var lastEnterTime = 0;
+		var nodesInfo = new (int lowTime, int discoverTime, int? parentIndex)[graph.Size];
+		var reversalStack = new Stack<(int currentIndex, int neigbourIndex)>();
+		
+		var joints = new List<TNode>();
+		var bridges = new List<(TNode from, TNode to)>();
+		
+		for (var i = 0; i < graph.Size; i++)
+		{
+			if (!visited.Contains(graph.Nodes[i]))
+			{
+				using var enumerator = new DFSEnumerator<TNode, TData>(graph, graph.Nodes[i], visited, stack, 
+					(current, g, s, v) => OnPrepareStackChanges(
+						current, g, s, v, ref lastEnterTime, nodesInfo, reversalStack, bridges, joints));
+				while (enumerator.MoveNext()) { }
+				while (reversalStack.Count > 0)
+				{
+					var pair = reversalStack.Pop();
+					nodesInfo[pair.currentIndex] = (
+						lowTime: Math.Min(nodesInfo[pair.currentIndex].lowTime, nodesInfo[pair.neigbourIndex].lowTime),
+						nodesInfo[pair.currentIndex].discoverTime,
+						nodesInfo[pair.currentIndex].parentIndex);
+
+					if (nodesInfo[pair.neigbourIndex].lowTime > nodesInfo[pair.currentIndex].discoverTime)
+					{
+						bridges.Add((graph.Nodes[pair.currentIndex], graph.Nodes[pair.neigbourIndex]));
+					}
+				}
+			}
+		}
+
+		return (joints, bridges);
+		static void OnPrepareStackChanges(
+            DFSEnumerator<TNode, TData>.DFSNode current, IGraph<TNode, TData> graph, 
+            Stack<DFSEnumerator<TNode, TData>.DFSNode> stack, HashSet<TNode> visited, 
+            ref int lastEnterTime,
+            (int lowTime, int discoverTime, int? parentIndex)[] nodesInfo,
+            Stack<(int currentIndex, int neighbourIndex)> reversalStack,
+            List<(TNode from, TNode to)> bridges,
+            List<TNode> joints)
+        {
+            visited.Add(current.node);
+            var currentNodeIndex = graph.GetIndex(current.node)!.Value;
+            nodesInfo[currentNodeIndex] = (lowTime: lastEnterTime, discoverTime: lastEnterTime, 
+	            nodesInfo[currentNodeIndex].parentIndex);
+            lastEnterTime++;
+            
+            var children = 0;
+            for (var adjIndex = 0; adjIndex < graph.Size; adjIndex++)
+            {
+	            if (graph[current.node][adjIndex].HasValue)
+	            {
+		            var neighbor = graph.Nodes[adjIndex];
+		            if (adjIndex == nodesInfo[currentNodeIndex].parentIndex) continue;
+		            if (visited.Contains(neighbor))
+		            {
+			            nodesInfo[currentNodeIndex] = (
+				            lowTime: Math.Min(nodesInfo[currentNodeIndex].lowTime, nodesInfo[adjIndex].discoverTime),
+				            nodesInfo[currentNodeIndex].discoverTime,
+				            nodesInfo[currentNodeIndex].parentIndex);
+		            }
+		            else
+		            {
+			            nodesInfo[adjIndex] = (nodesInfo[adjIndex].lowTime, nodesInfo[adjIndex].discoverTime, currentNodeIndex);
+			            children++;
+			            
+			            stack.Push(new DFSEnumerator<TNode, TData>.DFSNode(neighbor, current.depth + 1));
+			            reversalStack.Push((currentNodeIndex, adjIndex));
+
+			            if (nodesInfo[currentNodeIndex].parentIndex == null && children > 1)
+			            {
+				            joints.Add(current.node);
+			            }
+			            else if (nodesInfo[currentNodeIndex].parentIndex != null &&
+							nodesInfo[adjIndex].lowTime >= nodesInfo[currentNodeIndex].discoverTime)
+			            {
+				            joints.Add(current.node);
+			            }
+		            }
+	            }
+            }
+        }
+	}
 }
